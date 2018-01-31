1bc5aa82aa4d17aec89a2118c6236256a41af617
CallaJun <calla.carter@yahoo.com>
2018-01-30 22:52:41 -0500

    Implemented Euclidean distance method

--- main.py
+++ main.py
@@ -14,28 +14,44 @@ def create_connection(db_file):
         print(e)
     return None
 
-def select_all_tasks(connection):
+def select_data(connection):
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
     cur = connection.cursor()
-    cur.execute("SELECT * FROM Fires")
- 
-    rows = cur.fetchall()
-    print(len(rows))
+
+    # Printing out each row from Fires
+    #cur.execute("SELECT * FROM Fires")
+    #rows = cur.fetchall()
+    #print(len(rows))
+
+    cur.execute("SELECT FIRE_SIZE,LATITUDE,LONGITUDE FROM Fires")
+    data = cur.fetchall()
+    for row in data:
+        print("size: ", row[0],
+            " lati: ", row[1],
+            " long: ", row[2])
     #for row in rows:
     #    print(row)
 
+def euclidean_distance(point1, point2):
+    sum = 0
+    from math import pow
+    from cmath import sqrt
+    for i in xrange(len(point1)):
+        sum += pow(float(point1[i]) - float(point2[i]), 2)
+    return abs(sqrt(sum))
+
 def main():
-    database = "FPA_FOD_20170508.sqlite"
+    database = "wildfires.sqlite"
  
     # create a database connection
     connection = create_connection(database)
     with connection:
         print("Query all Fires")
-        select_all_tasks(connection)
+        select_data(connection)
 
 if __name__ == '__main__':
     main()
